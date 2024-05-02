455709d587ac9d8f3259a294a8d975423262ffc7
DaffyDandaniel <75188209+DaffyDandaniel@users.noreply.github.com>
2024-05-02 20:00:26 +0100

    Update and rename testing.txt to testing.lua

---
+++ testing.lua
@@ -0,0 +1,138 @@
+["Whitelist"] = {
+		-- // THE PRO'S \\ --
+		25642927, -- TanCoolCamish
+		112672616, -- AlexG_1337
+		199830788, -- Coco_Beagle
+		88605917, -- DanielF_1337
+		1713906111, -- Whitehill Holdings
+		
+		-- // WHITELIST \\ --
+		52675599, -- vaxhall123
+		138100978, -- DaffyDandaniel
+		108849299, -- Mxnastery
+		123570775, -- crazydoge2016
+		117413389, -- Joehale
+		107969816, -- RPerry435
+		
+		201210606, -- SlushyMax0215 - Giveaway winner (afternoon show)
+		1713386819, -- SnugglyKitten55555 - Competition winner (April fools event)
+		
+		-- // PAYPAL WHITELIST \\ --
+		4160674505, -- pescoholdingsacc
+		3995518959, -- thefreshmarketmanag
+		3985903287, -- JTPlayzRBLX2022
+		176438881, -- dunbars123
+		2459959254, -- UNlONPAClFlC
+		3368610683, -- Ne3sl
+		1122021603, -- Me100mm
+		1058684750, -- RealMrSmarts (Formerly kxren_14)
+		2225793730, -- CreatorHadley
+		123734937, -- cflegg
+		3284442274, -- JonathanOnTheWay
+		502610315, -- najlepszego
+		1900627424, -- Olz_999
+		2238550742, -- DamianS201209
+		12577343, -- CTT8330 (Original licence from thelovlygamemaker)
+		714875974, -- Happy_B33z
+		1811812794, -- matus2501
+		393365169, -- ThePiglinPlayz
+		4085843626, -- thomsonc12
+		468669804, -- shieldz123
+		323583601, -- jordan13k
+		966493266, -- nyoutchi123
+		
+		-- // LICENCE TRANSFERS \\ --
+		3304658698, -- LidlHoldings (Original licence from DanielF_1337)
+		3758306391, -- RescoHolder (Original licence from JettersAFC1)
+		218790800, -- ThomasEvanz (Original licence from cyuiao)
+		3808360939, -- forestview_holder (Original licence from Rhyz_07)
+		3868594084, -- AxtroHoldings (Original licence from Axtro_Remade)
+		4463101220, -- ArcaneHoldings (Original licence from Wolfy4358_Yt)
+		1091158996, -- Dq2rk (Original licence from ALDI_GroupManager)
+		4243286629, -- freshmartholder (Original licence from FJDavis1)
+		416918260, -- CreatorYo120 (Original licence from kuba2020rt)
+		220289927, -- hangname (Original licence from AgentFoxWolf)
+		366671978, -- QuadroFur (Original licence from cflegg)
+		4439902971, -- LukeHilton_Holder (Original licence from laughing2457)
+		2200462402, -- AvxLucasx (Original licence from HiitsEmma1234)
+		4430573891, -- TurnerHoldings (Original licence from ZaneTahaB)
+		5299619208, -- FinleyCharlton (Original licence from Lucasconwayy) - xlzcas_mainx > finleycharlton 08/12/23
+		1004225632, -- hype2704 (Original licence from Electrified_Pearl)
+		4743541963, -- Coopbloxgroupholder (Original licence from 27kxfading)
+		4924074913, -- Decsholder (Original licence from decallsop2008_3)
+		4872892798, -- ConnorDavidsons (Original licence from TheReventium [Paid part on both accounts])
+		2031322036, -- Jghfjdhdueg (Original licence from mmuchkin03 [Paid part on both accounts])
+		4974402192, -- Freshmartgroupholder (Original licence from ImBlake47456)
+		4729878040, -- uglyburger231 (Original licence from Malekbro112)
+		5058250660, -- MerutaHoldings (Original licence from XIgorekyt5684X)
+		787227386, -- noonecares21311j (Original licence from noonecares21311)
+		3640009573, -- olinicetoyou (Original licence from BIGTASTIELOL)
+		579844640, -- ludvig_katt (Original licence from Freinet123)
+		1469588769, -- Nathi_131 (Original licence from KendallJXK)
+		--2557421834, -- jaydeniamrich2 (Original licence from Ilikeeeeee0) -> TRANSFER REVOKED (racism)
+		5014223033, -- SunsetValley_Group (Original licence from warlikcookie82)
+		378872685, -- BaysideChurch (Originial licence from sxlithedollyx)
+		305481567, -- robloxrules756 (Original licence from LexiDeloris)
+		5553487835, -- IWonderMart (Original licence from Sajmon6612331)
+		4451454777, -- GoldStoreHolder (Original licence from HisMajesty_KingAiden)
+		862854020, -- willliamh (Original licence from BIGECZ12545)
+		160892610, -- LukeSco77 (Original licence from superstorefan055)
+		5590198665, -- AldiBlox_Holdings (Original licence from Ryantjuuh14)
+		3062307010, -- deniss494949 (Original licence from Ilovecomputers0)
+		5627554322, -- harvvxr (Original licence from Harvv15)
+		1935196177, -- H201_14 (Original licence from redst0ne8)
+		5447972415, -- GreenMart_Owner (Original licence from avramiote)
+		5627554322, -- harvvxr (Originial licence from Harvv15)
+		489418534, -- jqmeswxnder (Original licence from ENTERTAINMENT_HOLD)
+		4847661096, -- HolderVito (Original licence from Viittoo30)
+		5772487541, -- EverspectaHolder (Original licence from thinknoodlesfan8000)
+		4670408295, -- ImNotCsiklaoliver (Original licence from cooldude4235132)
+		4364415817, -- RealM12Gyt (Original licence from millarvanilla)
+	},
+	["Blacklist"] = {
+		2411306972, -- PratchCoolGamer (Copying Tan)
+		847164322, -- pratchlock25 (Second account Copying Tan)
+		274130466, -- hhhhhhhhyys (Voilating T&L rules and exploiting)
+		777215908, -- lewisnuter1234 (T&L Leaker)
+		3013747234, -- lekonus (T&L, Whitehill, Vocovo Leaker)
+		--1653117514, -- DANKEYZX (Copying Vocovo assets)
+		--3546625819, -- ctriggz (Leaking game containing Vocovo, appealed)
+		--2910453191, -- tavlnt (Leaking game containing Vocovo with ctriggz, appealed and tan accepted)
+		2557421834, -- jaydeniamrich2 (racism, major disrespect, even banned in whitehill)
+		5241595115, -- Ilikeeeeee0 (alt of ^^^ jaydeniamrich2 ^^^)
+		
+		----- DRS BLACKLIST -----
+		14790582, -- werdvon124 (DRS CEO)
+		130214198, -- NathanPlays2016 (DRS CO-CEO)
+		174880525, -- Modder0Source (DRS Support)
+		--471302710, -- Water_Osity (DRS Support)
+		--975327795, -- lllol234 (DRS Support)
+		--254090301, -- ll1uke (DRS Support)
+		--1972788081, -- VR_NEWS (DRS Support)
+		3022123594, -- ohwe_n (DRS Support)
+		541636051, -- Milky_GT (DRS Support)
+		--861750554, -- oneisonetoomany (DRS Support)
+		--130459376, -- carltheraveyt (DRS Support)
+		--1259543237, -- CameronTheGamer99 (DRS Support)
+		247480799, -- killerclownLV (DRS Support)
+		3801270430, -- DRS_Management (DRS Developer)
+		--173952075, -- mesmeriised (DRS Developer)
+		--1393279067, -- banana314444 (DRS Developer)
+		
+		----- "Axion" Corporation BLACKLIST -----
+		1596818555, -- DontYouOof (Ownership)
+		1784905130, -- cobnoob1234 (Managing Director)
+		564811849, -- eledontlie (Managing Director)
+		--787227386, -- noonecares21311j (Managing Director) (Appealed, very decent guy tan accepted)
+		778783786, -- Falcons_Fury (Managing Director)
+		631714497, -- curly_freckle (Managing Director)
+		1754069733, -- not_R0bert (Managing Director)
+	},
+	["GAuth"] = {
+		25642927, -- TanCoolCamish
+		112672616, -- AlexG_1337
+		199830788, -- Coco_Beagle
+		114001587, -- CreatedShots
+		88605917, -- DanielF_1337
+		138100978, -- DaffyDandaniel (sus)
+	},
